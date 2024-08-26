5cd8361a6b9ea80f849e514293cb557d0a1c22c4
Zohar Ifrah <cpxzoar@gmail.com>
2024-08-26 17:42:24 +0300

    add loader component and service, implement loader during contact deletion

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,7 +10,7 @@ import { provideHttpClient } from '@angular/common/http';
 import { ContactFilterComponent } from './cmps/contact-filter/contact-filter.component';
 import { FormsModule } from '@angular/forms';
 import { ContactDetailsComponent } from './pages/contact-details/contact-details.component';
-import { ChartComponent } from './cmps/charts/chart.component';
+import { ChartComponent } from './cmps/chart/chart.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { StatisticPageComponent } from './pages/statistic-page/statistic-page.component';
 import { HeaderComponent } from './cmps/header/header.component';
@@ -18,6 +18,7 @@ import { PageNotFoundComponent } from './cmps/page-not-found/page-not-found.comp
 import { ContactEditPageComponent } from './pages/contact-edit-page/contact-edit-page.component';
 import { NgxChartsModule } from '@swimlane/ngx-charts';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoaderComponent } from './cmps/loader/loader.component';
 
 
 
@@ -34,7 +35,8 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
     StatisticPageComponent,
     HeaderComponent,
     PageNotFoundComponent,
-    ContactEditPageComponent
+    ContactEditPageComponent,
+    LoaderComponent
   ],
   imports: [
     BrowserModule,
--- src/app/pages/contact-page/contact-page.component.ts
+++ src/app/pages/contact-page/contact-page.component.ts
@@ -2,7 +2,8 @@ import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import { ContactService } from '../../services/contact.service';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
 import { Contact } from '../../models/contact.model';
-import { Observable } from 'rxjs';
+import { delay, Observable } from 'rxjs';
+import { LoaderService } from '../../services/loader.service';
 
 @Component({
   selector: 'contact-page',
@@ -12,14 +13,17 @@ import { Observable } from 'rxjs';
 export class ContactPageComponent {
 
   private contactService = inject(ContactService)
+  private loaderService = inject(LoaderService)
   private destroyRef = inject(DestroyRef)
 
   contacts$: Observable<Contact[]> = this.contactService.contacts$
 
   onRemoveContact(contactId: string) {
-    console.log(contactId);
-    this.contactService.deleteContact(contactId).subscribe({
-      error: err => console.log('err:', err)
+    this.loaderService.setIsLoading(true)
+    this.contactService.deleteContact(contactId)
+    .subscribe({
+      error: err => console.log('err:', err),
+      complete: () => this.loaderService.setIsLoading(false)
     })
   }
 }
